4152e93be101f45240a6950484f33d95baa7cd43
Adamx2di <adam.didi@enis.tn>
2024-05-30 02:18:37 +0200

    Update sidbarMolds.js

--- JS/sidbarMolds.js
+++ JS/sidbarMolds.js
@@ -3,13 +3,13 @@ fetch("./molds.json")
 .then(data => {
     const moldsMenu = document.getElementById("moldMenu")
     const model = document.getElementById("3d_model")
-    model.setAttribute('gltf-model', ./moldsAssets/${data.names[0]}.glb)
+    model.setAttribute('gltf-model', `./moldsAssets/${data.names[0]}.glb`)
     data.names.forEach(name => {
         const link = document.createElement('a');
         link.textContent = name
         link.onclick = () => {
             model.removeAttribute('gltf-model');
-            model.setAttribute('gltf-model', ./moldsAssets/${name}.glb);
+            model.setAttribute('gltf-model', `./moldsAssets/${name}.glb`);
         }
         moldsMenu.appendChild(link)
     });
